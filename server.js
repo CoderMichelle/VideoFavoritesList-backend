ae598bc0f367f118b362fdd458f6ff8c89ddc562
mrpresent1@gmail.com <codermichelle@DESKTOP-89FL1O1.localdomain>
2022-02-19 20:08:07 -0800

    adds error handling via try catch and if else statement for api call

--- server.js
+++ server.js
@@ -29,19 +29,34 @@ async function getMovies(req, res) {
   // const query = req.query;
   const { movieName } = req.query;
   console.log('query is currently', movieName);
-  let movieURI = `https://api.themoviedb.org/3/search/movie?query=${movieName}&api_key=${process.env.MOVIE_DB_API_KEY}&language=en-US&page=1&include_adult=false`;
-  let movieResults = await axios.get(movieURI);
-  const movieResultsArray = movieResults.data.results.length
-    ? movieResults.data.results.map(
-      eachMovieObject => new Movie(eachMovieObject)
-    )
-    : [];
-  console.log('here are them movie results ARRAY', movieResultsArray);
-  let imdbMovieURI = `https://imdb-api.com/en/API/Trailer/${process.env.IMDB_API_KEY}/tt1375666`;
-  let imdbResults = await axios.get(imdbMovieURI);
-  console.log('here is IMDB =====================>>>>>>>', imdbResults.data);
-
-  res.status(200).send(movieResultsArray);
+  if (movieName) {
+    try {
+      let movieURI = `https://api.themoviedb.org/3/search/movie?query=${movieName}&api_key=${process.env.MOVIE_DB_API_KEY}&language=en-US&page=1&include_adult=false`;
+      let movieResults = await axios.get(movieURI);
+      const movieResultsArray = movieResults.data.results.length
+        ? movieResults.data.results.map(
+          eachMovieObject => new Movie(eachMovieObject)
+        )
+        : [];
+      console.log('here are them movie results ARRAY', movieResultsArray);
+      let imdbMovieURI = `https://imdb-api.com/en/API/Trailer/${process.env.IMDB_API_KEY}/tt1375666`;
+      let imdbResults = await axios.get(imdbMovieURI);
+      console.log('here is IMDB =====================>>>>>>>', imdbResults.data);
+      res.status(200).send(movieResultsArray);
+    } catch (error) {
+      console.log(
+        'there was an error trying to get the movies from the API',
+        error
+      );
+      res.status(404).send(error);
+    }
+  } else {
+    res
+      .status(500)
+      .send(
+        'you did not provide a valid search term (movieName=THE_MOVIE_YOU_ARE_SEARCHING_FOR'
+      );
+  }
 }
 
 class Movie {
