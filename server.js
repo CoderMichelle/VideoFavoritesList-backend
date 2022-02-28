9355cc838c365122a7691ffb8045519806d99846
mrpresent1@gmail.com <codermichelle@DESKTOP-89FL1O1.localdomain>
2022-02-28 15:32:35 -0800

    adds different CORS origin allowed origin : https://my-favorite-movies.netlify.app

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 const cors = require('cors');
 const corsOptions = {
   // origin: 'http://localhost:3000',
-  origin: 'https://my-favorite-movies.netlify.app/',
+  origin: 'https://my-favorite-movies.netlify.app',
   credentials: true,            //access-control-allow-credentials:true
   optionSuccessStatus: 200
 };
